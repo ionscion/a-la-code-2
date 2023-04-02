01a441a8cef8a0b9b0e6d2e375cbcee6386236b5
bensboto <bensboto@gmail.com>
2023-04-02 17:04:38 -0600

    added routes and auth config

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import LoginButton from "./login";
+import LogoutButton from "./logout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain=""
+      clientId=""
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <LoginButton />
+      <LogoutButton />
+      <RouterProvider router={router} />
+      
+    </Auth0Provider>
   </React.StrictMode>
 );
---
+++ src/routes/root.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import SidebarPage from "../pages/SidebarPage";
+
+function Root() {
+  return (
+    <div>
+      <Header />
+      <SidebarPage />
+    </div>
+  );
+}
+
+export default Root;
